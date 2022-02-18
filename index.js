0562d7ffec64ea7246f5aad8413061b66f8668a6
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-02-17 17:23:31 -0800

    implement fetchCoordsByIP

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+
+fetchCoordsByIP("199.116.243.197", (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log("It worked! Returned coordinates: ", coordinates);
+});
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
--- iss.js
+++ iss.js
@@ -23,5 +23,21 @@ const fetchMyIP = function(callback) {
   });
 };
 
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
-module.exports = { fetchMyIP };
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude } );
+  })
+};
+
+
+module.exports = { fetchCoordsByIP };
